81668646807d5e07e1cce3a8554308cbc7291e4e
Abby Acourt <Abigail.acourt@bglgroup.co.uk>
2018-08-02 16:16:07 +0100

    Resetting the Quiz pt 2

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -9,7 +9,7 @@ class QuizEnd extends Component {
     return (
       <div>
         <p>Thanks for playing!</p>
-        <a onClick = {this.handleResetClick.bind.this} href="">Reset Quiz</a>
+        <a onClick = {this.handleResetClick.bind(this)} href="">Reset Quiz</a>
       </div>
     );
   }
